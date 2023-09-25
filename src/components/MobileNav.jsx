79c6f5a820fb8ac11ec5e13d1ff9aba97f621e1e
ryangholland <rghcode@gmail.com>
2023-09-25 15:03:52 -0400

    Add functionality to MobileNav component

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,6 +1,8 @@
 import { Drawer, Button, Box } from "@mui/material";
 import { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 
 function MobileNav() {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,8 +16,8 @@ function MobileNav() {
       <Drawer anchor="left" open={isOpen} onClose={() => setIsOpen(false)}>
         <Box
           sx={{
-            width: "250px",
-            textAlign: "center",
+            width: "200px",
+
             p: 4,
             display: "flex",
             flexDirection: "column",
@@ -29,6 +31,70 @@ function MobileNav() {
           >
             X
           </Button>
+          <nav>
+            <Box sx={{ display: "flex", flexDirection: "column" }}>
+              <Link
+                component={RouterLink}
+                to="/products/all"
+                variant="button"
+                color="inherit"
+                href="#"
+                sx={{ my: 1, mx: 1.5 }}
+                underline="hover"
+                onClick={() => setIsOpen(false)}
+              >
+                All
+              </Link>
+              <Link
+                component={RouterLink}
+                to="/products/womens"
+                variant="button"
+                color="inherit"
+                href="#"
+                sx={{ my: 1, mx: 1.5 }}
+                underline="hover"
+                onClick={() => setIsOpen(false)}
+              >
+                Women's
+              </Link>
+              <Link
+                component={RouterLink}
+                to="/products/mens"
+                variant="button"
+                color="inherit"
+                href="#"
+                sx={{ my: 1, mx: 1.5 }}
+                underline="hover"
+                onClick={() => setIsOpen(false)}
+              >
+                Men's
+              </Link>
+              <Link
+                component={RouterLink}
+                to="/products/jewelry"
+                variant="button"
+                color="inherit"
+                href="#"
+                sx={{ my: 1, mx: 1.5 }}
+                underline="hover"
+                onClick={() => setIsOpen(false)}
+              >
+                Jewelry
+              </Link>
+              <Link
+                component={RouterLink}
+                to="/products/electronics"
+                variant="button"
+                color="inherit"
+                href="#"
+                sx={{ my: 1, mx: 1.5 }}
+                underline="hover"
+                onClick={() => setIsOpen(false)}
+              >
+                Electronics
+              </Link>
+            </Box>
+          </nav>
         </Box>
       </Drawer>
     </>
